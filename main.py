f29b1d4a7a5a1f26738392d12f0b8d220ed7074e
ujwal <ujwalkirsan2003@gmail.com>
2024-04-15 01:10:00 +0530

    added flask in main file

--- main.py
+++ main.py
@@ -34,6 +34,28 @@ def query_data_and_send_emails(df):
             email_counter += 1
     return f"Total Emails Sent: {email_counter}"        
 
-df = load_df(URL)
-result = query_data_and_send_emails(df)
-print(result)
+from flask import Flask
+ 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+ 
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    df = load_df(URL)
+    result = query_data_and_send_emails(df)
+    return result
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
+
+
+
